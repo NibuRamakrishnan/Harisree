24e5979a642876ff797a0e59307087241fcbc873
Nibu R <90435990+NibuRamakrishnan@users.noreply.github.com>
2024-10-19 19:02:16 +0530

    Add files via upload

--- assets/js/royal.js
+++ assets/js/royal.js
@@ -23,5 +23,45 @@ $(document).ready(function(){
             $("#particulars").val(itemtext);
         }
         $("#particularsList").css("display","none");
-    }) 
+    });
+    $("#btnadd").on("click", function(){
+        var item = $("#particulars").val();
+        var descr=$("#descr").val();
+        var amount=$("#amount_exp").val();
+        var source=$("#cash_from_exp").val();
+        var ID=Date.now().toString(36) + Math.random().toString(36).substring(2);
+        if(item != "" && amount > 0 && source != "")
+        {
+            let jsonData = 
+            {
+                "expenses":[{
+                "ID":ID,
+                "item": item,
+                "descr": descr,
+                "amount": amount,
+                "source": source
+                }]
+            };
+            let jsonString = JSON.stringify(jsonData);
+            console.log(jsonString);
+            if(jsonString != null &&  jsonString != "")
+            {
+                createTable(jsonString);
+            }
+        }
+    });
   });
+  function createTable(jsonString){
+    var jsondata = JSON.parse(jsonString);
+    var html_content = ""; 
+    $.each(jsondata.expenses, function(i, v){
+        var ID = jsondata.expenses[i].ID;
+        var item = jsondata.expenses[i].item;
+        var amount =  jsondata.expenses[i].amount;
+        var source =  jsondata.expenses[i].source; 
+        html_content= html_content + ("<tr><th identifier='"+ID+"' scope='row'> <a class='link'><i class='fa fa-edit'></i></a> <a class='link'><i class='fa fa-trash'></i></a> </th> <td key='particulars'>"+item+"</td> <td key='amount_exp'>"+amount+"</td> <td key='cash_from_exp'>"+source+"</td></tr>");
+    });
+    if(html_content != ""){
+        $("#tbl_tempdata_expense tbody").append(html_content);
+    }
+  }
